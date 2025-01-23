1eddb06ab9cc4759fbfdb3ecebaac794a02f338a
pati-rosa <patirosa995@gmail.com>
2025-01-23 15:55:51 -0300

    feat: create my wallet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchImages, uploadImage } from './firebase'; // Ou o caminho do seu arquivo
+import { Input, Button } from '@mui/material';
+function  App() {
 
-function App() {
+  let fetchTopClothes = fetchImages("top-clothes")
+  let fetchUnderwear = fetchImages("underwear")
+  console.log('fetch',fetchTopClothes,fetchUnderwear)
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Meu guarda roupa virtual
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+         Adicione peças de roupas e monte looks
+        </p>
+        <div className="button-container">
+          <Button variant="contained" component="label" >
+          {"Adicionar parte de cima"}
+            <input hidden type="file" onChange={(e) => uploadImage(e.target.files[0],'top-clothes')} id='uploadUpImage'/>
+          </Button>
+
+          <Button variant="contained" component="label" >
+          {"Adicionar parte de baixo"}
+          <input hidden type="file" onChange={(e) => uploadImage(e.target.files[0],'underwear')} id='uploadUnderwearImage'/>        
+          </Button>
+        </div>
       </header>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL, listAll } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtMgNns29p_GfZiDifXmWTUkaGPaHb7aY",
+  authDomain: "meu-guardaroupa.firebaseapp.com",
+  projectId: "meu-guardaroupa",
+  storageBucket: "meu-guardaroupa.firebasestorage.app",
+  messagingSenderId: "622419432631",
+  appId: "1:622419432631:web:d9aed7f24801ac99932932",
+  measurementId: "G-D9KLQBDG4F"
+};
+
+initializeApp(firebaseConfig);
+
+export const uploadImage = async (file, clothingType) => {
+  console.log('clothingType',clothingType)
+    const storage = getStorage();
+    const storageRef = ref(storage, `${clothingType}/${file.name}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    console.log('uploadTask',uploadTask)
+
+    uploadTask.on('state_changed',(snapshot) => {},(error) => {
+      console.log('error',error)
+    },() => {
+      
+        // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log('File available at', downloadURL);
+      });
+    });   
+}
+
+export const fetchImages = async (clothingType) => {
+  try {
+    const storage = getStorage();
+    const folderRef = ref(storage, clothingType); 
+
+    const result = await listAll(folderRef);
+
+    const imageUrls = await Promise.all(
+      result.items.map(async (itemRef) => {
+        const url = await getDownloadURL(itemRef);
+        return url;
+      })
+    );
+
+    console.log('Image URLs:', imageUrls);
+    return imageUrls; // Retorna a lista de URLs para exibir as imagens no seu app
+
+  } catch (error) {
+    console.error("Error fetching images:", error);
+  }
+}
