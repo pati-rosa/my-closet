8f90d9798adc923218c24e41967078f255633230
pati-rosa <patirosa995@gmail.com>
2025-01-25 19:24:03 -0300

    chore: use env variables on firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "meu-guardaroupa.firebaseapp.com",
-  projectId: "meu-guardaroupa",
-  storageBucket: "meu-guardaroupa.firebasestorage.app",
-  messagingSenderId: "622419432631",
-  appId: "1:622419432631:web:d9aed7f24801ac99932932",
-  measurementId: "G-D9KLQBDG4F"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
