cf47d64b4dbfe34f088361f95825aa9baba19be9
pati-rosa <patirosa995@gmail.com>
2025-01-25 18:52:50 -0300

    feat: carousel moves with mouse and touch

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { carouselContainer, imageContent, button } from './style';
 
 interface CarouselProps {
@@ -7,24 +7,88 @@ interface CarouselProps {
 }
 
 const Carousel: React.FC<CarouselProps> = ({ images, imageState }) => {
-    const [curentIndex, setCurentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [startTouch, setStartTouch] = useState(0);
+    const [isDragging, setIsDragging] = useState(false);
+
+    const carouselRef = useRef<HTMLDivElement>(null);
 
     const handlePrevClick = () => {
-        setCurentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
     };
 
     const handleNextClick = () => {
-        setCurentIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : prevIndex));
+        setCurrentIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
+    };
+
+    const handleMouseDown = (e: React.MouseEvent) => {
+        setIsDragging(true);
+        setStartTouch(e.clientX); 
+    };
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        if (!isDragging) return;
+    };
+
+    const handleMouseUp = (e: React.MouseEvent) => {
+        console.log('mouse up',e.clientX);
+        if (!isDragging) return;
+        const endTouch = e.clientX;
+        if (endTouch - startTouch > 50) {
+            handlePrevClick(); 
+        } else if (startTouch - endTouch > 50) {
+            handleNextClick();
+        }
+        setIsDragging(false);
+    };
+
+    const handleMouseLeave = (e: React.MouseEvent) => {
+        if (!isDragging) return;
+        const endTouch = e.clientX;
+        if (endTouch - startTouch > 50) {
+            handlePrevClick();
+        } else if (startTouch - endTouch > 50) {
+            handleNextClick();
+        }
+        setIsDragging(false);
+    };
+
+    const handleTouchStart = (e: React.TouchEvent) => {
+        setStartTouch(e.touches[0].clientX);
+    };
+
+    const handleTouchMove = (e: React.TouchEvent) => {
+        if (!isDragging) return;
+    };
+
+    const handleTouchEnd = (e: React.TouchEvent) => {
+        const endTouch = e.changedTouches[0].clientX;
+        if (endTouch - startTouch > 50) {
+            handlePrevClick();
+        } else if (startTouch - endTouch > 50) {
+            handleNextClick();
+        }
+        setIsDragging(false);
     };
 
     return(
-        <div style={carouselContainer}>
+        <div 
+            style={carouselContainer}
+            ref={carouselRef}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}  
+            onMouseLeave={handleMouseLeave}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+        >
             {imageState === 'loading' && <p>Carregando...</p>}
             {imageState === 'error' && <p>Erro ao carregar as imagens</p>}
             {imageState === 'success' && (
             <>
                 <button style={button} onClick={handlePrevClick}>{'<'}</button>
-                <img src={images[curentIndex]} style={imageContent}/>
+                <img draggable={false} src={images[currentIndex]} style={imageContent}/>
                 <button style={button} onClick={handleNextClick}>{'>'}</button>
             </>)
             }
